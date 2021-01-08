704204972deaebfe721b0346d29784ab73eb5b05
jean-godoy <70454706+jean-godoy@users.noreply.github.com>
2021-01-08 10:36:16 -0300

    Update Kernel.php

--- src/Kernel.php
+++ src/Kernel.php
@@ -35,4 +35,11 @@ class Kernel extends BaseKernel
             (require $path)($routes->withPath($path), $this);
         }
     }
+    
+    public function registerBundles()
+    {
+        $bundles = [
+            new Nelmio\CorsBundle\NelmioCorsBundle(),
+        ];
+    }
 }
